1674bad898cd0f58f0d727bab3db2e954724de06
Andrzej Derela <andrzejderela@gmail.com>
2021-06-05 10:32:10 +0200

    Add navbar, gallery lightbox and contact form

--- run.py
+++ run.py
@@ -7,22 +7,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', title='Home')
 
 
 @app.route('/gallery')
 def gallery():
-    return render_template('gallery.html')
+    return render_template('gallery.html', title='Gallery')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About Me')
 
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', title='Contact')
+
+
+@app.route('/thankyou')
+def thankyou():
+    return render_template('thankyou.html', title='Thank You!')
 
 
 @app.errorhandler(404)
